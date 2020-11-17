9793a3f9260d2208d21461defc9e491127c47edd
Cristiam Da Silva <cristiam86@gmail.com>
2020-11-17 18:00:40 +0100

    added helper function to show rounded amounts

--- src/helpers/filecoin.js
+++ src/helpers/filecoin.js
@@ -3,4 +3,8 @@ const ROUND_DECIMALS = process.env.REACT_APP_ROUND_DECIMALS;
 export const formatAddress = (address) => `f${address.slice(1)}`;
 
 export const absoluteAmount = (amount) => (amount * 1000000000000000000).toString();
-export const friendlyAmount = (amount) => (amount / 1000000000000000000).toFixed(ROUND_DECIMALS);
+export const friendlyAmount = (amount, roundDecimals = ROUND_DECIMALS) => (amount / 1000000000000000000).toFixed(roundDecimals);
+
+export const autoRound = amount => {
+  return String(parseFloat(friendlyAmount(amount, 18)));
+}
